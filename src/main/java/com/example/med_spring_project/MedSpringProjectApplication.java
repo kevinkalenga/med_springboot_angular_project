b38cc7bb52f66e5eb76b26eb2c2fea2c83f3901e
kevin <kevinkalenga10@gmail.com>
2026-08-17 12:17:51 +0200

    Create Project

---
+++ src/main/java/com/example/med_spring_project/MedSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.med_spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/med_spring_project/MedSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.med_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
