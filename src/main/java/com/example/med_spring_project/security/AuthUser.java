8a511d0610036e941ed563c31f4bdf7ea97e25fd
kevin <kevinkalenga10@gmail.com>
2026-08-31 15:17:00 +0200

    Auth User

---
+++ src/main/java/com/example/med_spring_project/security/AuthUser.java
@@ -0,0 +1,38 @@
+package com.example.med_spring_project.security;
+
+import com.example.med_spring_project.role.entity.Role;
+import com.example.med_spring_project.users.entity.User;
+import jakarta.annotation.Nullable;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+@Data
+public class AuthUser implements UserDetails {
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .toList();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
