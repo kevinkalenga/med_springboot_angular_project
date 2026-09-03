dfad32a5d5e92eafe38e5b897a8236ce1403c814
kevin <kevinkalenga10@gmail.com>
2026-09-03 14:41:47 +0200

    Auth Filter

---
+++ src/main/java/com/example/med_spring_project/security/AuthFilter.java
@@ -0,0 +1,87 @@
+package com.example.med_spring_project.security;
+
+import com.example.med_spring_project.exception.CustomAuthenticationEntryPoint;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class AuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        String token = getTokenFromRequest(request);
+        String email = null;
+        if (token != null) {
+
+
+            try {
+                email = jwtService.getUsernameFromToken(token);
+
+                // Ici tu peux charger l'utilisateur
+                // et créer le SecurityContext
+
+            } catch (Exception ex) {
+                log.error("Exception occurred while extracting username from token", ex);
+
+                AuthenticationException authenticationException =
+                        new BadCredentialsException(ex.getMessage(), ex);
+
+                customAuthenticationEntryPoint.commence(
+                        request,
+                        response,
+                        authenticationException
+                );
+
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+
+        if(StringUtils.hasText(email) && jwtService.isTokenValid(token, userDetails)) {
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null,
+                    userDetails.getAuthorities());
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+        
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String tokenWithBearer = request.getHeader("Authorization");
+
+        if (tokenWithBearer != null && tokenWithBearer.startsWith("Bearer ")) {
+            return tokenWithBearer.substring(7);
+        }
+
+        return null;
+    }
+}
